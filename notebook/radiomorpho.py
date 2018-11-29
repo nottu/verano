75f2218b1780f2d8a3ee3e11fd873cb350d3471f
Javier Peralta <francisco.peralta@cimat.mx>
2018-11-29 15:25:49 -0600

    new Kittler, and LBP

--- notebook/radiomorpho.py
+++ notebook/radiomorpho.py
@@ -90,3 +90,75 @@ def readImagesFromDirs(dirs):
           print('error reading image {0} or {1}'.format(im[0], im[1]))
   files = names
   return files, np.array(images)
+
+
+# thresholding code
+def threshold_kittler(img, e = 10E-6, m_iter=100):
+  def new_hist_T(p, a, v):
+    #-b +- sqrt( pow(b, 2) - 4ac)/ 2
+    _a = (1/v[0] - 1/v[1])
+    _b = -2 * (a[0]/v[0] - a[1]/v[1])
+    _c = a[0]**2 / v[0] - a[1]**2 / v[1]
+    _c += 2 * (np.log(np.sqrt(v[0])) - np.log(np.sqrt(v[1])))
+    _c -= 2 * (np.log(p[0]) - np.log(p[1]))
+
+    pos = ( -_b + np.sqrt(_b**2 - 4*_a*_c))/(2*_a)
+    return pos
+
+  def get_params(img, T, mask=None):
+    mask = img > T
+    mask_i = img < T
+    p = (float(img[mask_i].shape[0])/(img.shape[0] * img.shape[1]),
+         float(img[mask].shape[0])/(img.shape[0] * img.shape[1]))
+    a = (img[mask_i].mean(), img[mask].mean())
+    v = (img[mask_i].var(), img[mask].var())
+    return(p,a,v)
+
+  T = img.mean()
+  new_t = 0
+  while( m_iter ):
+    m_iter -= 1
+    new_t = new_hist_T(*get_params(img, T))
+    if abs(new_t - T) < e: break
+    else: T = new_t
+  return new_t
+
+def Kittler(im, n=8):
+    """
+    The reimplementation of Kittler-Illingworth Thresholding algorithm by Bob Pepin
+    Works on n-bit images only
+    Original Matlab code: https://www.mathworks.com/matlabcentral/fileexchange/45685-kittler-illingworth-thresholding
+    Paper: Kittler, J. & Illingworth, J. Minimum error thresholding. Pattern Recognit. 19, 41–47 (1986).
+    """
+    bits=2**n
+    h,g = np.histogram(im.ravel(),bits,[0,bits])
+    h = h.astype(np.float)
+    g = g.astype(np.float)
+    g = g[:-1]
+    c = np.cumsum(h)
+    m = np.cumsum(h * g)
+    s = np.cumsum(h * g**2)
+    sigma_f = np.sqrt(s/c - (m/c)**2)
+    cb = c[-1] - c
+    mb = m[-1] - m
+    sb = s[-1] - s
+    sigma_b = np.sqrt(sb/cb - (mb/cb)**2)
+    p =  c / c[-1]
+    v = p * np.log(sigma_f) + (1-p)*np.log(sigma_b) - p*np.log(p) - (1-p)*np.log(1-p)
+    v[~np.isfinite(v)] = np.inf
+    idx = np.argmin(v)
+    t = g[idx]
+    return t
+
+def Kittler_flot(im, n=16):
+  return Kittler(im*(2**n), n) / (2**n)
+
+def remove_data_bellow_threshold(img, threshold_alg=Kittler_flot, ratio=40):
+  disk_sz = 1 + int(img.shape[0]/ratio)
+  mask = img > threshold_alg(img)
+  # mask = sk.morphology.opening(mask, sk.morphology.disk(disk_sz))
+  img_o = np.zeros_like(img)
+  for i in range(img.shape[0]):
+      for j in range(img.shape[1]):
+          img_o[i, j] = img[i, j] if(mask[i, j]) else 0
+  return img_o
